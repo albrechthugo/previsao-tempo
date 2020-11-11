1de4d1b3c27a355dc1f2253d2ae39bf7971634fd
Hugo Albrecht <albrechthugo4@gmail.com>
2020-11-10 21:50:46 -0300

    refatorando design da previsao das capitais

--- src/components/Capitals/index.jsx
+++ src/components/Capitals/index.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 import './index.css';
 
+import capitals from '../../utils/capitals';
+import request from '../../services/api';
+
 const Capitals = () => {
     return (
         <div className="capitals__container">
-            <h1>Capitais</h1>
+            <h1>Previsão nas capitais</h1>
             <div className="capitals__row">
                 <div className="capitals__content">
-                    <p>Min</p>
-                    <p>Máx</p>
-                </div>
-                <div className="capitals__content">
-                    <p>Min</p>
-                    <p>Máx</p>
+                    <p>Rio de Janeiro</p>
+                    <span>15°</span>
+                    <span>28°</span>
                 </div>
             </div>
         </div>
