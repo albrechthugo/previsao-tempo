fccb47ba9e26071fcc0133877f829c3eda350ad2
Hugo Albrecht <albrechthugo4@gmail.com>
2020-11-10 21:50:57 -0300

    array de capitais para fazer as chamadas a api

---
+++ src/utils/capitals.js
@@ -0,0 +1,12 @@
+const capitals = [
+    'Rio de Janeiro',
+    'Florianópolis',
+    'São Paulo',
+    'Rio Branco',
+    'Goiânia',
+    'Curitiba',
+    'Porto Alegre',
+    'Belo Horizonte',
+];
+
+export default capitals;
